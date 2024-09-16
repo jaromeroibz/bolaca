6da807880bab71c31dfd4b10028c7262626d337d
jaromeroibz <137736111+jaromeroibz@users.noreply.github.com>
2024-09-16 19:42:09 +0000

    falta terminar responsiveness de todos los componentes

--- src/front/js/component/shoppingCart.js
+++ src/front/js/component/shoppingCart.js
@@ -13,51 +13,61 @@ export const ShoppingCart = () =>{
     // }
     const shippingCost = 3000;
     const shippingPrice = itemsPrice > 20000 ? 0 : shippingCost;
-    const totalPrice = shippingCost + shippingPrice;
+    const totalPrice = itemsPrice + shippingPrice;
     return(
         <>
-        <div className="container py-5">
+        <div className="container">
             {store.cart.length === 0 ? <h1>Tu carrito está vacío</h1>
             :
                 <div className="cart">
-                    <h1>Tu carrito</h1>
-                        <div className="row">
-                            {store.cart.map((item) =>
-                            <div className="col-8">
-                                <div className="card" style={{width: 800, height: 100}}>
-                                    <div className="d-flex">
-                                        <div className="px-5">
-                                            <img className="card-img-top" src="https://picsum.photos/200/200" alt="Card image cap" style={{width: 60, height: 60}}></img>
-                                        </div>
-                                        <h6 className="px-5">{item.name}</h6>
-                                        <div className="qty px-5">
-                                        {item ? (
-                                            <div>
-                                                <button onClick={() => actions.removeFromCart(item)}>-</button>
-                                                <span className="p-1">{item.qty}</span>
-                                                <button onClick={() => actions.addToCart(item)}>+</button>
+                    <h3>Tu carrito</h3>
+                        <div className="d-xl-inline-flex py-3">
+                            <div className="card product-list">
+                                {store.cart.map((item) =>
+                                <div className="py-3">
+                                    <div className="cart-product" style={{width: 800, height: 100}}>
+                                        <div className="d-flex">
+                                            <div className="px-xs-2 px-sm-3 px-md-4 px-lg-5">
+                                                <img className="card-img-top" src="https://picsum.photos/200/200" alt="Card image cap" style={{width: 60, height: 60}}></img>
                                             </div>
-                                        ) : (
-                                            <button onClick={() => actions.addToCart(item)}></button>
-                                        )
-                                        }
+                                            <h6 className="px-xs-2 px-sm-3 px-md-4 px-lg-5">{item.name}</h6>
+                                            <div className="qty px-xs-2 px-sm-3 px-md-4 px-lg-5">
+                                            {item ? (
+                                                <div>
+                                                    <button className="qty-btn border-0 shadow-none" onClick={() => actions.removeFromCart(item)}>-</button>
+                                                    <span className="p-1">{item.qty}</span>
+                                                    <button className="qty-btn border-0 shadow-none" onClick={() => actions.addToCart(item)}><i class="fa-regular fa-plus"></i></button>
+                                                </div>
+                                            ) : (
+                                                <button onClick={() => actions.addToCart(item)}></button>
+                                            )
+                                            }
+                                            </div>
+                                            <div className="total-price px-xs-2 px-sm-3 px-md-4 px-lg-5">${item.price*item.qty}</div>
                                         </div>
-                                        <div className="total-price px-5">${item.price*item.qty}</div>
-                                        <hr></hr>
+                                        <hr className="custom-hr"></hr>
                                     </div>
                                 </div>
+                                )
+                            }
                             </div>
-                            )
-                        }
-                            <div className="col-4 resumen-compra">
-                                <div className="card">
-                                    <h3>Resumen compra</h3>
+                            <div className="px-5">
+                                <div className="card py-3 py-md-3 px-5" style={{width: 450, height: 300}}>
+                                    <h3 className="mx-auto">Resumen compra</h3>
                                     <hr></hr>
-                                    <div><p>Productos ({store.cart.length}) {itemsPrice}</p></div>
-                                    <div className="d-flex"></div>
-                                    <div><p>Costo de envío {shippingCost}</p></div>
-                                    <div><p>Total {totalPrice}</p></div>
-                                    <button>Ir a pagar</button>
+                                    <div className="d-flex justify-content-between">
+                                        <p>Productos: ({store.cart.length})</p>
+                                        <p>{itemsPrice}</p>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <p>Costo de envío:</p>
+                                        <p>{shippingPrice}</p>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <p>Total:</p>
+                                        <p>{totalPrice}</p>
+                                    </div>
+                                    <button className="add-cart-button">Ir a pagar</button>
                                 </div>
                             </div>
                         </div>
