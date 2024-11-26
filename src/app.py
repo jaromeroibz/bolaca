df10c19966bb5ef518d1c230ba6fc737e6a47d9c
jaromeroibz <137736111+jaromeroibz@users.noreply.github.com>
2024-11-26 22:28:03 +0000

    deploy frontend

--- src/app.py
+++ src/app.py
@@ -21,7 +21,7 @@ from flask_jwt_extended import JWTManager
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
-app = Flask(__name__, static_folder="src/front/dist", static_url_path="")
+app = Flask(__name__, static_folder="dist", static_url_path="")
 app.url_map.strict_slashes = False
 
 # database condiguration
