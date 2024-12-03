a5e55f0714185e5f2bee8c9fe856f592d80f8f1b
jaromeroibz <137736111+jaromeroibz@users.noreply.github.com>
2024-12-03 02:06:18 +0000

    vamoo

--- webpack.common.js
+++ webpack.common.js
@@ -4,52 +4,51 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = {
-  entry: [
-    './src/front/build/static/js/index.js'
-  ],
+  entry: './src/front/build/static/js/index.js', // Adjusted to a simpler format
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/'
+    filename: 'bundle.js', // The output file name
+    path: path.resolve(__dirname, 'build'), // Output folder (ensure this folder exists or is created)
+    publicPath: '/', // Ensures that the bundle.js is correctly loaded from root
   },
   devServer: {
     static: {
-      directory: path.resolve(__dirname, "build"),
+      directory: path.resolve(__dirname, 'build'), // Ensure correct directory for serving static files
     },
     historyApiFallback: true, // Ensures React routing works
-    port: 3000,
+    port: 3000, // Port to run the dev server
   },
   module: {
     rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
+      {
+        test: /\.(js|jsx)$/, 
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(css|scss)$/, 
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|jpeg|webp)$/, 
+        use: {
+          loader: 'file-loader',
+          options: { name: '[name].[ext]' },
         },
-        {
-          test: /\.(css|scss)$/, use: [{
-              loader: "style-loader" // creates style nodes from JS strings
-          }, {
-              loader: "css-loader" // translates CSS into CommonJS
-          }]
-        }, //css only files
-        {
-          test: /\.(png|svg|jpg|gif|jpeg|webp)$/, use: {
-            loader: 'file-loader',
-            options: { name: '[name].[ext]' }
-          }
-        }, //for images
-        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] } //for fonts
-    ]
+      },
+      { 
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, 
+        use: ['file-loader'] 
+      },
+    ],
   },
   resolve: {
-    extensions: ['.jsx', '.js']
+    extensions: ['.jsx', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
-        template: 'template.html'
+      favicon: '4geeks.ico',
+      template: 'template.html', // Make sure this path is correct
     }),
-    new Dotenv({ safe: true, systemvars: true })
-  ]
+    new Dotenv({ safe: true, systemvars: true }),
+  ],
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,20 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+
 module.exports = merge(common, {
-    mode: 'production',
-    output: {
-        publicPath: '/'
+  mode: 'production',
+  output: {
+    // If you want to set specific options for production build, do it here
+  },
+  optimization: {
+    minimize: true, // Minify the output for production
+    splitChunks: {
+      chunks: 'all', // Split common chunks to optimize caching
     },
-    plugins: [
-        new Dotenv({
-            safe: true,
-            systemvars: true
-        })
-    ]
+  },
+  plugins: [
+    new Dotenv({ safe: true, systemvars: true }), // Ensure your environment variables are correctly loaded
+  ],
 });
+
