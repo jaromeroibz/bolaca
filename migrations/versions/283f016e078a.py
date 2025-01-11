ccb910daea7c8e0d9d49f76afe6ad7c4a139fc6c
jaromeroibz <137736111+jaromeroibz@users.noreply.github.com>
2025-01-11 18:03:42 +0000

    lala

---
+++ migrations/versions/283f016e078a.py
@@ -0,0 +1,24 @@
+"""Merge conflicting heads
+
+Revision ID: 283f016e078a
+Revises: 
+Create Date: 2025-01-11 17:57:58.736215
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '283f016e078a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
