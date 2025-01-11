9e658c3ac211d45497587e7e66b80ce6ddd5001a
jaromeroibz <137736111+jaromeroibz@users.noreply.github.com>
2025-01-11 17:40:49 +0000

    db reset

---
+++ migrations/versions/d02b38db6e8f.py
@@ -0,0 +1,18 @@
+"""Placeholder migration for d02b38db6e8f"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# Revision identifiers, used by Alembic.
+revision = 'd02b38db6e8f'
+down_revision = None  # Change this if you know the actual previous revision ID
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    """This is a placeholder upgrade method."""
+    pass
+
+def downgrade():
+    """This is a placeholder downgrade method."""
+    pass
