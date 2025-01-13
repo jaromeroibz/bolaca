45e8f886a76fa4ea556e5251778a4874f0e5afa4
jaromeroibz <137736111+jaromeroibz@users.noreply.github.com>
2025-01-13 17:55:06 +0000

    changed to ES module syntax

--- server.mjs
+++ server.mjs
@@ -1,17 +1,18 @@
-const express = require("express");
-const path = require("path");
+import express from 'express';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the React build directory
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(process.cwd(), 'build')));
 
 // Serve the index.html file for any unknown routes
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(process.cwd(), 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
